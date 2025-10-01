598817eb9916bd47a2ef0351b38e71c4bf4aba6b
Phoenixmorrisville <mamrak387@morrisville.edu>
2025-10-01 07:36:05 -0400

    Added delayed respawn using Invoke()

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -3,12 +3,17 @@ using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] float reloadDelay = 1f;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         int layerIndex = LayerMask.NameToLayer("Floor");
         if (collision.gameObject.layer == layerIndex)
         {
-            SceneManager.LoadScene(0);
+            Invoke("ReloadScene", reloadDelay);
         }
+    }
+    private void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
